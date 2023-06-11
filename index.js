86b6ad4d64617de702ccc39067fbc57be7141f8a
manjurul17002 <manjurul17002@gmail.com>
2023-06-11 14:57:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors')
+const app =express()
+require("dotenv").config();
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) =>{
+    res.send('language server is running')
+})
+
+
+
+app.listen(port, () =>{
+    console.log(`language is running on port: ${port}`);
+})
