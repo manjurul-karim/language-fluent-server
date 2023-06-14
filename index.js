8034f29ecc493949b608320ec5dec5bed13be01d
manjurul17002 <manjurul17002@gmail.com>
2023-06-14 22:21:00 +0600

    jwt

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+//  ! vefify jwt
+
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
